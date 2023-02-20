58aee9cabc22dd614e4aabc993ed35442fdce07e
Jacob Welander <Jacobwelander1994@gmail.com>
2023-02-20 18:01:19 +0100

    adds styling to the p tag

--- src/components/projectPage.vue
+++ src/components/projectPage.vue
@@ -21,8 +21,9 @@ import glitchText from './glitchText.vue';
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste facilis amet dicta quod deserunt modi,
             necessitatibus at iure quibusdam officia voluptate fugit voluptatem eligendi! Pariatur veritatis reiciendis
             excepturi magni error?
-
         </p>
+
+
     </div>
 </template>
 
@@ -33,15 +34,14 @@ import glitchText from './glitchText.vue';
 }
 
 .box {
-    margin: 0 2vw;
-    min-height: 90vh;
+    margin: 0 0 2vw 2vw;
+    min-height: 85vh;
     border: white 2px solid;
     border-color: white transparent transparent white;
 
     p {
         padding: 15px 0 0 20px;
         color: white;
-
     }
 
 }
