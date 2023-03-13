ccca7482eafb14022109ff075a75f115d2318a00
Jacob Welander Jensen <64834767+Welander1994@users.noreply.github.com>
2023-03-13 11:23:00 +0100

    adds github action

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,7 +12,7 @@ import menu_bar from '../components/menu/menu.vue'
   <main>
     <div class=" main__title">
       <!--      <h1 id="Web__title">Jacob Welander</h1> -->
-      <glitchText class="gradient-text" title="Web Developer Student" />
+      <glitchText class="gradient-text" title="test Web Developer Student" />
       <p>
         I'm a web development student at UCL in Odense, and I'm passionate about building engaging and effective
         websites. Take a look at my <RouterLink to="/projects">projects</RouterLink> to see some examples of my work, or
